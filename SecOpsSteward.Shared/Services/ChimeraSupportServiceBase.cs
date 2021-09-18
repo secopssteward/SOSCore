52028e6dc40a092ecead04f0f66bf454eb50d6bf
Anthony Turner <225599+anthturner@users.noreply.github.com>
2021-09-18 16:23:45 -0400

    code

---
+++ SecOpsSteward.Shared/Services/ChimeraSupportServiceBase.cs
@@ -0,0 +1,13 @@
+﻿namespace SecOpsSteward.Shared.Services
+{
+    /// <summary>
+    ///     Denotes a Chimera integrated service which has tasks to execute
+    /// </summary>
+    public interface IChimeraIntegratedService
+    {
+        /// <summary>
+        ///     Priority to use when organizing all system services (lower is earlier execution)
+        /// </summary>
+        int ServicePriority { get; }
+    }
+}
